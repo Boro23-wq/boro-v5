8a6e55e093b3ea6c5d4d489217eb430159212a07
Sintu Boro <sintuboro@Sintus-MBP.lan>
2023-08-01 14:12:24 -0500

    added resume

--- components/connect/index.js
+++ components/connect/index.js
@@ -1,4 +1,4 @@
-import { GitHub, Mail, Twitter, Linkedin } from '@components/icons'
+import { GitHub, Mail, Twitter, Linkedin, Download } from '@components/icons'
 import Link from '@components/link'
 
 import styles from './connect.module.css'
@@ -8,37 +8,48 @@ const ConnectData = () => {
     <>
       <p className={styles.heading}>Get in touch.</p>
 
-      <Link
-        href="https://github.com/Boro23-wq"
-        external
-        className={styles.link}
-      >
-        <GitHub size={24} />
-      </Link>
+      <div className={styles.links}>
+        <Link
+          href="https://github.com/Boro23-wq"
+          external
+          className={styles.link}
+        >
+          <GitHub size={24} />
+        </Link>
 
-      <Link
-        href="https://twitter.com/sdotboro"
-        external
-        className={styles.link}
-      >
-        <Twitter size={24} />
-      </Link>
+        <Link
+          href="https://twitter.com/sdotboro"
+          external
+          className={styles.link}
+        >
+          <Twitter size={24} />
+        </Link>
 
-      <Link
-        href="mailto:sboro2899@gmail.com?subject=Lets connect!"
-        external
-        className={styles.link}
-      >
-        <Mail size={24} />
-      </Link>
+        <Link
+          href="mailto:sboro2899@gmail.com?subject=Lets connect!"
+          external
+          className={styles.link}
+        >
+          <Mail size={24} />
+        </Link>
 
-      <Link
-        href="https://linkedin.com/in/sintu-boro"
-        external
-        className={styles.link}
-      >
-        <Linkedin size={24} />
-      </Link>
+        <Link
+          href="https://linkedin.com/in/sintu-boro"
+          external
+          className={styles.link}
+        >
+          <Linkedin size={24} />
+        </Link>
+
+        <Link
+          href="/assets/boro_resume.pdf"
+          external
+          download
+          className={styles.link}
+        >
+          <Download size={24} />
+        </Link>
+      </div>
     </>
   )
 }
